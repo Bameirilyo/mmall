3c919a60bbbe642a03ac2cc1d31cb4deac014721
bameirilyo <1218702044@qq.com>
2018-06-11 19:52:25 +0800

    搜索List页改造RESTful

--- src/main/java/com/mmall/controller/portal/ProductController.java
+++ src/main/java/com/mmall/controller/portal/ProductController.java
@@ -4,6 +4,7 @@ import com.github.pagehelper.PageInfo;
 import com.mmall.common.ServerResponse;
 import com.mmall.service.IProductService;
 import com.mmall.vo.ProductDetailVo;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -58,5 +59,117 @@ public class ProductController {
                                          @RequestParam(value = "pageSize",defaultValue = "10")int pageSize,
                                          @RequestParam(value = "orderBy",defaultValue = "")String orderBy){
         return iProductService.getProductByKeywordCategory(keyword,categoryId,pageNum,pageSize,orderBy);
+    }
+
+    /**
+     * 搜索List页改造RESTful
+     * @param keyword
+     * @param categoryId
+     * @param pageNum
+     * @param pageSize
+     * @param orderBy
+     * @return
+     */
+    //http://localhost:8080/product/手机/100012/1/10/price_asc/
+    @RequestMapping(value = "/{keyword}/{categoryId}/{pageNum}/{pageSize}/{orderBy}",method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<PageInfo> listRESTful(@PathVariable(value = "keyword")String keyword,
+                                         @PathVariable(value = "categoryId")Integer categoryId,
+                                         @PathVariable(value = "pageNum")Integer pageNum,
+                                         @PathVariable(value = "pageSize")Integer pageSize,
+                                         @PathVariable(value = "orderBy")String orderBy){
+        if (pageNum == null){
+            pageNum = 1;
+        }
+        if (pageSize == null){
+            pageNum = 10;
+        }
+        if (StringUtils.isBlank(orderBy)){
+            orderBy = "price_asc";
+        }
+
+        return iProductService.getProductByKeywordCategory(keyword,categoryId,pageNum,pageSize,orderBy);
+    }
+
+    //    http://localhost:8080/product/手机/1/10/price_asc/
+    // java.lang.IllegalStateException: Ambiguous handler methods mapped for HTTP path
+    @RequestMapping(value = "/{categoryId}/{pageNum}/{pageSize}/{orderBy}",method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<PageInfo> listRESTfulBadcase(@PathVariable(value = "categoryId")Integer categoryId,
+                                                @PathVariable(value = "pageNum")Integer pageNum,
+                                                @PathVariable(value = "pageSize")Integer pageSize,
+                                                @PathVariable(value = "orderBy")String orderBy){
+        if (pageNum == null){
+            pageNum = 1;
+        }
+        if (pageSize == null){
+            pageNum = 10;
+        }
+        if (StringUtils.isBlank(orderBy)){
+            orderBy = "price_asc";
+        }
+
+        return iProductService.getProductByKeywordCategory("",categoryId,pageNum,pageSize,orderBy);
+    }
+
+    //    http://localhost:8080/product/手机/1/10/price_asc/
+    // java.lang.IllegalStateException: Ambiguous handler methods mapped for HTTP path
+    @RequestMapping(value = "/{keyword}/{pageNum}/{pageSize}/{orderBy}",method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<PageInfo> listRESTfulBadcase(@PathVariable(value = "keyword")String keyword,
+                                                @PathVariable(value = "pageNum")Integer pageNum,
+                                                @PathVariable(value = "pageSize")Integer pageSize,
+                                                @PathVariable(value = "orderBy")String orderBy){
+        if (pageNum == null){
+            pageNum = 1;
+        }
+        if (pageSize == null){
+            pageNum = 10;
+        }
+        if (StringUtils.isBlank(orderBy)){
+            orderBy = "price_asc";
+        }
+
+        return iProductService.getProductByKeywordCategory(keyword,null,pageNum,pageSize,orderBy);
+    }
+
+    //http://localhost:8080/product/keyword/手机/1/10/price_asc/
+    @RequestMapping(value = "/keyword/{keyword}/{pageNum}/{pageSize}/{orderBy}",method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<PageInfo> listRESTful(@PathVariable(value = "keyword")String keyword,
+                                                       @PathVariable(value = "pageNum")Integer pageNum,
+                                                       @PathVariable(value = "pageSize")Integer pageSize,
+                                                       @PathVariable(value = "orderBy")String orderBy){
+        if (pageNum == null){
+            pageNum = 1;
+        }
+        if (pageSize == null){
+            pageNum = 10;
+        }
+        if (StringUtils.isBlank(orderBy)){
+            orderBy = "price_asc";
+        }
+
+        return iProductService.getProductByKeywordCategory(keyword,null,pageNum,pageSize,orderBy);
+    }
+
+    //http://localhost:8080/product/category/100012/1/10/price_asc/
+    @RequestMapping(value = "/category/{categoryId}/{pageNum}/{pageSize}/{orderBy}",method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<PageInfo> listRESTful(@PathVariable(value = "categoryId")Integer categoryId,
+                                                       @PathVariable(value = "pageNum")Integer pageNum,
+                                                       @PathVariable(value = "pageSize")Integer pageSize,
+                                                       @PathVariable(value = "orderBy")String orderBy){
+        if (pageNum == null){
+            pageNum = 1;
+        }
+        if (pageSize == null){
+            pageNum = 10;
+        }
+        if (StringUtils.isBlank(orderBy)){
+            orderBy = "price_asc";
+        }
+
+        return iProductService.getProductByKeywordCategory("",categoryId,pageNum,pageSize,orderBy);
     }
 }
